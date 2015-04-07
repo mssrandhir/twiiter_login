0f1b1ab2b5a59c649b1f115aed886847019de0e9
mss_randhir <mss.randhir@gmail.com>
2015-04-07 10:28:53 +0530

    Updated

--- lib/twitter_login.rb
+++ lib/twitter_login.rb
@@ -2,7 +2,17 @@ require "twitter_login/version"
 
 module TwitterLogin
   # Your code goes here...
-  	def self.init
-  		return "First out puts from gem"
+  class init
+
+	  	def initialize (app_key = nil, app_secret = nil)
+
+	  		if api_key and api_secret
+	  			return "Initilizer is working"
+	  		else
+	  			return "You must specify you app credentials"
+	  		end
+
+			end
+
 	end
 end
