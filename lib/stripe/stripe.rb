bcb4da1b1eee1f3ffe1ca1c93791c958edfa86d3
mss_randhir <mss.randhir@gmail.com>
2015-04-21 16:14:12 +0530

    Updated

---
+++ lib/stripe/stripe.rb
@@ -0,0 +1,8 @@
+module TwitterLogin
+  # VERSION = "0.1.0"
+  class StripeLibs
+  	def self.init
+  		return "include"
+  	end
+  end
+end
--- lib/twitter_login.rb
+++ lib/twitter_login.rb
@@ -1,13 +1,14 @@
 require "twitter_login/version"
+require "stripe/stripe"
 
 module TwitterLogin
   # Your code goes here...
   class InitAccessKeys
 
 	  	def self.initialize (app_key = nil, app_secret = nil)
-
+	  		TwitterLogin::StripeLibs.init
 	  		if app_key and app_secret
-	  			return "Initilizer is working" + "app key " + app_key + "app secret " + app_secret
+	  			return "Initilizer is working " + "app key " + app_key + "app secret " + app_secret
 	  		else
 	  			return "You must specify you app credentials"
 	  		end
